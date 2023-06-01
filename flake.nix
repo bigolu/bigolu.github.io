bf87eb4b2b11eaacbf460e79d658bfe9019a9685
Biggie Emmanuel <hi@bigo.lu>
2023-06-01 18:58:20 -0400

    add a Nix devshell with the version of node required for this project

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Biggs's personal site";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem
+      (with flake-utils.lib.system; [ x86_64-linux x86_64-darwin ])
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          devShells.default = pkgs.mkShell { packages = [pkgs.nodejs]; };
+        }
+      );
+}
