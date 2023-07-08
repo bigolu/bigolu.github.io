c0d9bc6d3c8063a614a50ae9071f001c8555bb42
Biggie Emmanuel <hi@bigo.lu>
2023-07-08 15:09:58 -0400

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,19 @@
 import styles from './page.module.css'
+import Image from 'next/image'
+import profilePicture from './static_biggs.png'
 
 export default function About() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>This is the about page.</p>
+    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${styles.container}`}>
+      <Image
+        src={profilePicture}
+        alt="Picture of the author"
+        width={500}
+        height={500}
+      />
+      <div className={`nes-container ${styles['greeting-container']}`} style={{padding: 0}}>
+        <span>Hey how{"'"}s it going?</span>
+      </div>
     </main>
   )
 }
