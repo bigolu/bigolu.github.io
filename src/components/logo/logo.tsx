b9ca383b4750223b13deff0054b84920bb97e25d
Biggie Emmanuel <hi@bigo.lu>
2023-07-10 19:34:16 -0400

    make logo link back to home

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -2,10 +2,12 @@ import styles from './logo.module.css'
 
 export default function LogoComponent() {
   return (
-    <div>
-      <span>ola</span><span>olu</span>
-      <br></br>
-      <span>big</span><span>gie</span>
-    </div>
+    <a href='/'>
+      <div>
+        <span>ola</span><span>olu</span>
+        <br></br>
+        <span>big</span><span>gie</span>
+      </div>
+    </a>
   )
 }
