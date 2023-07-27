e8d62ee5344af3d930a4443490c918712e0259e7
Biggie Emmanuel <hi@bigo.lu>
2023-07-27 13:26:42 -0400

    tweak header and colors

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link'
 export default function NavbarComponent() {
   return (
     <div className={styles.container}>
-      <Link href='/about'>About</Link>
-      <Link href='/code'>Code</Link>
+      <Link href='/about'>about</Link>
+      <Link href='/code'>code</Link>
     </div>
   )
 }
